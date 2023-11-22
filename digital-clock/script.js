b9e88468f4c3f44d5129afc2d6caf62f271acb68
Alfian Maulana <alfianmna@gmail.com>
2023-11-22 08:23:07 +0700

    add digital-clock

---
+++ digital-clock/script.js
@@ -0,0 +1,30 @@
+const hourE1 = document.getElementById("hour");
+const minuteE1 = document.getElementById("minutes");
+const secondE1 = document.getElementById("seconds");
+const ampmE1 = document.getElementById("ampm");
+
+function clock() {
+  let h = new Date().getHours();
+  let m = new Date().getMinutes();
+  let s = new Date().getSeconds();
+  let ampm = "AM";
+
+  if (h > 12) {
+    h = h - 12;
+    ampm = "PM";
+  }
+
+  h = h < 10 ? "0" + h : h;
+  m = m < 10 ? "0" + m : m;
+  s = s < 10 ? "0" + s : s;
+
+  hourE1.innerText = h;
+  minuteE1.innerText = m;
+  secondE1.innerText = s;
+  ampm, (innerText = ampm);
+  setTimeout(() => {
+    clock();
+  }, 1000);
+}
+
+clock();
